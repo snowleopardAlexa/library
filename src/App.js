8e0fc89dc7e045cb3e5a329d907b90964fde04c4
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-19 22:07:16 -0500

    add layout

--- src/App.js
+++ src/App.js
@@ -15,7 +15,29 @@ function App() {
     <div className="container">
       <div className="row justify-content-center">
         <div className="col">
-
+          <h1 className="fw-normal text-center my-3">Books</h1>
+          {books?.length > 0 ? (
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>id</th>
+                  <th>name</th>
+                  <th>year</th>
+                </tr>
+              </thead>
+              <tbody>
+                {books.map(({ id, name, year }) => (
+                  <tr key={id}>
+                    <td>{id}</td>
+                    <td>{name}</td>
+                    <td>{year}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+           ) : (  
+            <p>No books</p>
+          )}
         </div>
       </div>  
     </div>
