381cbce50b1eb5dbbd8a626efb6518c1edc0a509
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-27 20:33:42 -0500

    change character

--- src/server.js
+++ src/server.js
@@ -15,11 +15,11 @@ createServer ({
     const yen = server.create("character", { name: "Yennefer from Vengerberg"})
     const oz = server.create("character", { name: "Jackson Oz"})
     const mitch = server.create("character", { name: "Mitch Morgan"})
-    const hermy = server.create("character", { name: "Hermionie Granger"})
+    const anna = server.create("character", { name: "Anna Karenina"})
 
     server.create("book", { name: "Zoo", year: 2010, characters: [oz, mitch] })
     server.create("book", { name: "Witcher: The Sword of Destiny", year: 2014, characters: [geralt, yen] })
-    server.create("book", { name: "Anna Karenina", year: 1878, characters: [hermy] })
+    server.create("book", { name: "Anna Karenina", year: 1878, characters: [anna] })
   },
   routes() {
     this.namespace = "api"
