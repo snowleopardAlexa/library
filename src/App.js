e9bdcb235d67e5b58ffe3f7dce68a2bd90edebd1
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-27 20:09:52 -0500

    add modal

--- src/App.js
+++ src/App.js
@@ -159,6 +159,24 @@ const setBookToUpdate = (id) => {
           )}
         </div>
       </div>
+      <div className="modal fade" tabindex="-1" id="exampleModal">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Characters</h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+            
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
